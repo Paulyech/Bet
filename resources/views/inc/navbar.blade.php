68a4e9163e3a69c3e2d0ddb6d73925ecc390c688
Paulyech <paulyech96@gmail.com>
2020-12-24 22:00:16 +0300

    bettail

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,40 @@
+<header class="bg-gray-800 lg:px-16 lg:py-0 py-2 flex flex-wrap items-center">
+    <div class="flex flex-1 justify-between items-center ">
+        <a href="">
+            <svg class="w-12 h-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5" />
+              </svg>
+        </a>
+    </div>
+
+        <label for="menu-toggle" class="lg:hidden cursor-pointer block bg-gray-700 mr-4 rounded-lg border-white border-2 ">
+            <svg class="w-12 h-10 fill-current  " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+        </label>
+        <input type="checkbox" id="menu-toggle" class="hidden">
+        <div id="menu" class="hidden lg:flex lg:items-center lg:w-auto w-full">
+            <nav>
+                <ul class="lg:flex justify-between items-center pt-4 lg:pt-0 text-base text-gray-400">
+                    <li>
+                        <a href="#" class="nav-link">home</a>
+                    </li>
+                    <li>
+                        <a href="#about" class="nav-link">about</a>
+                    </li>
+                    <li>
+                        <a href="#services" class="nav-link">services</a>
+                    </li>
+                    <li>
+                        <a href="#portfolio" class="nav-link">portfolio</a>
+                    </li>
+                    <li>
+                        <a href="#contact" class="nav-link">contact</a>
+                    </li>
+                    
+                </ul>
+            </nav>
+        </div>
+    
+</header>
+
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <title> {{config('app.name','Bet')}} </title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+
+        <style>
+            body {
+                font-family: 'Nunito';
+            }
+        </style>
+         <style>
+            #menu-toggle:checked + #menu{
+                display: block;
+            }
+        </style>
+    </head>
+    <body class="antialiased text-gray-600 bg-gray-900 ">
+        <div>
+         @include('inc.navbar')
+             <main class="py-6 px-12 ">    
+                 @yield('content')
+             </main>  
+          @include('inc.footer')
+ 
+       </div>
+    </body>
+</html>
