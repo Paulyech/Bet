f6371e67440c3626fb6559410a329add76d3f0fd
Paulyech <pauliech06@gmail.com>
2021-01-03 22:00:09 +0300

    logo

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,9 +1,7 @@
 <header class="bg-gray-800 lg:px-16 lg:py-0 flex flex-wrap items-center">
     <div class="flex flex-1 justify-between items-center ">
         <a href="">
-            <svg class="w-12 h-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5" />
-              </svg>
+            <img src="../images/logo.png" alt="logo" class="w-20 h-20 p-0">
         </a>
     </div>
 
